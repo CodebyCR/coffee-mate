7d268b51dbf026229385b72baf43d52e9fa94e45
Christoph <christoph_rohde@icloud.com>
2025-02-27 20:35:58 +0100

    Initial Commit

---
+++ CoffeemateTests/CoffeemateTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeemateTests.swift
+//  CoffeemateTests
+//
+//  Created by Christoph Rohde on 27.02.25.
+//
+
+import Testing
+@testable import Coffeemate
+
+struct CoffeemateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
